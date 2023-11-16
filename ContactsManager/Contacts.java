85398d60226235f0ad2614e521566ef9144ea29c
JenniferLA <park.jennifer2324@gmail.com>
2023-11-16 16:17:48 -0600

    added view contacts

--- ContactsManager/Contacts.java
+++ ContactsManager/Contacts.java
@@ -1,12 +1,9 @@
 package ContactsManager;
 
 import java.io.IOException;
-import java.lang.reflect.Constructor;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -20,27 +17,31 @@ public class Contacts {
     // Main line of code
     public void runContactsApp() {
 
+        // Create Path objects for the directory and file
+        Path pathToContacts = Paths.get("contacts.txt");
+        List<Contact> currentContacts = contactsUtils.loadContacts(pathToContacts);
+
         // Menu
         System.out.print("1. View contacts.\n" + "2. Add a new contact.\n" + "3. Search a contact by name.\n" + "4. Delete an existing contact.\n" + "5. Exit.\n" + "Enter an option (1, 2, 3, 4 or 5):\n");
 
         String userInput = input.nextLine();
 
+
+
         //Switch/Case
         switch (userInput) {
-            case 1 -> viewContacts();
-            case 2 -> addContact();
-            case 3 -> searchContact();
-            case 4 -> deleteContact();
-            case 5 -> System.exit(0);
+            case "1" -> contactsUtils.outputList(currentContacts);
+//            case "2" -> addContact();
+//            case "3" -> searchContact();
+//            case "4" -> deleteContact();
+            case "5" -> System.exit(0);
         }
 
 
-        // Create Path objects for the directory and file
-        Path pathToContacts = Paths.get("contacts.txt");
-        System.out.println(pathToContacts);
+//        System.out.println(pathToContacts);
 
-        List<String> currentContacts = contactsUtils.readFile(pathToContacts);
-        contactsUtils.outputList(currentContacts);
+
+//        contactsUtils.outputList(currentContacts);
 
         try {
             if (Files.notExists(pathToContacts)) {
@@ -50,24 +51,34 @@ public class Contacts {
             iox.printStackTrace();
         }
 
+
+        // 1. View Contacts
+
+
+
         //Append Contact
-        private void addContact() {
-            System.out.println("Enter a name");
-            String userAddName = input.nextLine();
-            System.out.println("Enter a number");
-            int userAddNumber = input.nextInt();
-            Contact contact = new Contact(userAddName, userAddNumber);
+//        private void addContact() {
+        // List contacts = loadConatcs()
 
-            List<String> moreContacts = Arrays.asList( );
-            try {
-                Files.write(pathToContacts, moreContacts, StandardOpenOption.APPEND);
-            } catch (IOException iox) {
-                System.out.println("Error writing to file " + iox.getMessage());
-            }
+//            System.out.println("Enter a name");
+//            String userAddName = input.nextLine();
+//            System.out.println("Enter a number");
+//            int userAddNumber = input.nextInt();
+//            Contact contact = new Contact(userAddName, userAddNumber);
+//              contacts.add(
 
-        }
+//            List<String> moreContacts = Arrays.asList( );
+//            try {
+//                Files.write(pathToContacts, moreContacts, StandardOpenOption);
+//            } catch (IOException iox) {
+//                System.out.println("Error writing to file " + iox.getMessage());
+//            }
+//
+//        }
 
     }
+
+
 
 
 }
