dc243359a86a4dd59113e5855f0ff7508c555c7c
JenniferLA <park.jennifer2324@gmail.com>
2023-11-17 15:28:04 -0600

    Added bonus

--- ContactsManager/Contact.java
+++ ContactsManager/Contact.java
@@ -20,11 +20,29 @@ public class Contact {
     }
 
     public String getNumber() {
-        return number;
+//        return number;
+        return formatPhoneNumber(number);
     }
 
     public void setNumber(String number) {
         this.number = number;
+    }
+
+    private String formatPhoneNumber(String phoneNumber) {
+        // Remove all non-digit characters from the phone number
+        String digitsOnly = phoneNumber.replaceAll("\\D", "");
+
+        // Check if the number has the required length for formatting
+        if (digitsOnly.length() == 10) {
+            // Format number with dashes (###) ###-####
+            return String.format("(%s) %s-%s",
+                    digitsOnly.substring(0, 3),
+                    digitsOnly.substring(3, 6),
+                    digitsOnly.substring(6));
+        } else {
+            // If the number doesn't have 10 digits, return the original number
+            return phoneNumber;
+        }
     }
 
     public String toString(){
